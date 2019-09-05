271bc01af5f4034dda53b9a146c4dec792f365ee
sriram12 <kallam.sriramreddy@gmail.com>
2019-09-06 01:14:43 +0530

    asian branch develop

--- src/Demo.java
+++ src/Demo.java
@@ -6,10 +6,13 @@ public class Demo {
 	public void run(){
 		System.out.println("after the git operation in gitdemo");
 	}
-		
+    public void type(){
+    	System.out.println("Asian guy modification in branch develop");
+    }	
 	public static void main(String [] args){
 	 Demo a = new Demo();
 	 a.run();
+	 a.type();
 		
 	}
 	}
--- src/Demo1.java
+++ src/Demo1.java
@@ -4,15 +4,17 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Demo1 {
 	
+	public void run(){
+		System.out.println("after the git operation in gitdemo");
+	}
+	public void develop(){
+		System.out.println("ment for develope branch changes");
+	}	
 	public static void main(String [] args){
-		System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe" );
-		WebDriver driver = new ChromeDriver();
-		driver.get("http://facebook.com");
-		//driver.findElement(By.id("email")).sendKeys("sriramreddy.kallam@gmil.com");
-		driver.findElement(By.cssSelector("input#email.inputtext")).sendKeys("sriramreddy.kallam@gmail.com");
-        driver.findElement(By.name("pass")).sendKeys("dollar hai");
-       // driver.findElement(By.linkText("Forgotten account?")).click();
-        driver.findElement(By.id("u_0_8")).click();
+	 Demo1 a = new Demo1();
+	 a.run();
+	 a.develop();
+		
 	}
 	
 
